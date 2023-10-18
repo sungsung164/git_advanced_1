c88d3fa3ee97e00e51c281d4977d6023cc98256d
sungsung164 <toodol135@gmail.com>
2023-10-18 20:53:11 +0900

    main initialized

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
     """
     Determines if a number is even and return an even list.
@@ -9,8 +10,10 @@ def even_list(int_list: List[int]) -> List[int]:
     Returns:
         A list of even integers.
     """
-    return [i for i in int_list if i % 2 == 0]
+    # TODO: Implement even_list
+    pass
 
+# Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     """
     Computes the sum of the squares of all even numbers in a list of integers.
@@ -21,11 +24,13 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     Returns:
         The sum of the squares of all even numbers in the list.
     """
-    return sum(i**2 for i in even_int_list)
+    # TODO: Implement sum_of_squares_of_even
+    pass
 
+# Main function
 def main():
     # Example list
-    int_list = [58, 6, -22, 91, 33, 49, 44, 63, -79, 12]
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     even_int_list = even_list(int_list)
     output = sum_of_squares_of_even(even_int_list)
     print(output)
